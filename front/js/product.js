140ed8aa1b97ff41f09ea5ebc4275ab2a3fcf8ae
kalene APIOU <apiou.kalene@gmail.com>
2021-10-11 11:17:18 +0200

    first commit

---
+++ front/js/product.js
@@ -0,0 +1,85 @@
+const fetchProduct = async () => {
+    const queryString_Url_id = window.location.search;
+    const product =
+        "http://localhost:3000/api/products/" + queryString_Url_id.slice(4);
+    await fetch(product)
+        .then((res) => res.json())
+        .then((data) => {
+            products = data;
+        })
+        .catch((error) => console.log(error));
+};
+const display = () => {
+    if (products.length === 0) {
+        console.log("Pas de produits");
+    } else {
+        const img = document.createElement("img");
+        const item_img = document.getElementsByClassName("item__img");
+
+        img.setAttribute("src", products.imageUrl);
+        img.setAttribute("alt", products.altTxt);
+
+        item_img[0].appendChild(img);
+
+        title.textContent = `${products.name}`;
+        price.textContent = `${(products.price / 100).toFixed(2)}`;
+        description.textContent = `${products.description}`;
+
+        let colorsProduct = [];
+        products.colors.map((color) => {
+            const option = document.createElement("option");
+            option.setAttribute("value", color);
+            option.textContent = color;
+            colors.appendChild(option);
+        });
+        colors.innerHTML += `${colorsProduct.join("")}`;
+    }
+};
+
+const addToCard = () => {
+    const addToCartId = document.getElementById("addToCart");
+
+    addToCartId.addEventListener("click", (e) => {
+        const color = document.getElementById("colors").value;
+        const quantity = parseInt(document.getElementById("quantity").value);
+        const productLocalStorageName = products.name + " color: " + color;
+
+        if (quantity > 100 || quantity === 0 || color === "") {
+            console.log("Choisissez une quantité entre 1-100 et une couleur");
+            alert("Choisissez une quantité entre 1-100 et une couleur");
+        } else if (localStorage.getItem(productLocalStorageName) === null) {
+            products.quantity = quantity;
+            products.color = color;
+            localStorage.setItem(
+                productLocalStorageName,
+                JSON.stringify(products)
+            );
+            console.log("test1");
+        } else if (localStorage.getItem(productLocalStorageName) !== null) {
+            products.quantity = parseInt(quantity);
+            products.color = color;
+            localStorage.setItem(
+                productLocalStorageName,
+                JSON.stringify(products)
+            );
+        } else {
+            console.log("Une erreur est survenue");
+        }
+    });
+    const allStorage = () => {
+        let values = [];
+        let keys = Object.keys(localStorage);
+        let i = keys.length;
+
+        for (let index = 0; index < i; index++) {
+            values.push(localStorage.getItem(keys[index]));
+        }
+        return values;
+    };
+    allStorage().forEach((element) => {
+        console.log(JSON.parse(element));
+    });
+};
+fetchProduct()
+    .then(() => display())
+    .then(() => addToCard());
---
+++ front/js/script.js
@@ -0,0 +1,38 @@
+const fetchProducts = async () => {
+    await fetch("http://localhost:3000/api/products")
+        .then((res) => res.json())
+        .then((data) => {
+            products = data;
+        })
+        .catch((error) => console.log(error));
+};
+const display = () => {
+    if (products.length === 0) {
+        console.log("Pas de produits");
+    } else {
+        products.map((product) => {
+            const href = document.createElement("a");
+            const article = document.createElement("article");
+            const img = document.createElement("img");
+            const h3 = document.createElement("h3");
+            const p = document.createElement("p");
+
+            href.setAttribute("href", "./product.html?id=" + product._id);
+            img.setAttribute("src", product.imageUrl);
+            img.setAttribute("alt", product.altTxt);
+            h3.className = "productName";
+            p.className = "productDescription";
+
+            h3.textContent  = product.name;
+            p.textContent  = product.description;
+
+            href.appendChild(article);
+            article.appendChild(img);
+            article.appendChild(h3);
+            article.appendChild(p);
+
+            document.getElementById("items").appendChild(href);
+        });
+    }
+};
+fetchProducts().then(() => display());
